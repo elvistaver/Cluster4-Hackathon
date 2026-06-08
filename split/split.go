2c1ed8dfe35993cd6a92257bdcb7809728eb3b87
elvistaver <elvistaver@gmail.com>
2026-06-08 15:27:40 +0100

    split

---
+++ split/split.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func split(text string)[]string{
+	result:= strings.Fields(text)
+	return result
+}
+
+func Split(text string)[]string{
+	result:=[]string{}
+
+	tempra:=[]rune{}
+
+	for _, chr:= range text{
+
+		if unicode.IsPunct(chr){
+			result=append(result, string(tempra))
+			tempra=[]rune{}
+		}
+
+		if chr!=' '{
+			tempra=append(tempra, chr)
+			continue
+		}
+		if len(tempra) != 0{
+			result = append(result, string(tempra))
+			tempra=[]rune{}
+			continue
+		}
+	}
+	return result
+}
+func main(){
+	fmt.Println(split("Hello, world! How are you?"))
+	fmt.Println(Split("Hello, world! How are you?"))
+}
